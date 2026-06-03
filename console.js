79b54cceab4127e2c54b82e0b059e67720d95306
Rimpledeep kaur <krimpledeep74@gmail.com>
2026-06-03 16:44:47 +0530

    Update console.js

--- console.js
+++ console.js
@@ -1,1 +1,11 @@
-console.log("Portfolio Loaded Successfully");
+var typed = new Typed("#typing", {
+    strings: [
+        "M.Tech Student",
+        "Machine Learning Enthusiast",
+        "Computer Vision Learner",
+        "Digital Image Processing Researcher"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
+});
