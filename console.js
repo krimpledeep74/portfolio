f89a526b97db298a826cad84c0c4f5983c27c0bc
Rimpledeep kaur <krimpledeep74@gmail.com>
2026-06-09 13:48:02 +0530

    Update console.js

--- console.js
+++ console.js
@@ -1,11 +1,7 @@
-var typed = new Typed("#typing", {
-    strings: [
-        "M.Tech Student",
-        "Machine Learning Enthusiast",
-        "Computer Vision Learner",
-        "Digital Image Processing Researcher"
-    ],
-    typeSpeed: 60,
-    backSpeed: 40,
-    loop: true
-});
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello Rimple</h1>
+    </main>
+  );
+}
